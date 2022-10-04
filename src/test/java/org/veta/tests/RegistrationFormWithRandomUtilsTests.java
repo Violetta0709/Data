3322d43935a783c8bef8c3b546eb99ea252fc8ec
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-10-04 22:53:01 +0300

    with random utils.

--- src/test/java/org/veta/tests/RegistrationFormWithRandomUtilsTests.java
+++ src/test/java/org/veta/tests/RegistrationFormWithRandomUtilsTests.java
@@ -3,7 +3,6 @@ package org.veta.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
